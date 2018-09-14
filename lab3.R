bba6f865f304de9703d5dbaa4cfed7ddeee17871
何嘉乐 <hejiale@wirelessprv-10-192-43-247.near.illinois.edu>
2018-09-14 17:31:34 -0500

    mid version lab3

--- lab3.R
+++ lab3.R
@@ -13,28 +13,22 @@ arrowdraw <- function(){
   arrows(x0=loc[[1]][1],y0=loc[[2]][1], x1=loc[[1]][2], y1=loc[[2]][2])
 }
 
-setwd("/Users/hejiale/499")
+setwd("/Users/hejiale/cpsc499")
 library(ggplot2)
 library(viridis)
 yieldGWAS <- read.csv("Msi_yield_GWAS.csv", stringsAsFactors = FALSE)
 str(yieldGWAS)
 mhtplt <- function(file, thresholdline = c(5,8)){
   ordered <- dplyr::arrange(file,Chromosome,Position)
-  ggplot(ordered,aes(x= Position, y= P.value))+
-    geom_bar()+
-    facet_grid(Chromosome~Position)+
-    xlab("positions")+
+  ordered$Reorder <- as.numeric(rownames(ordered))
+  # use sapply to get all biggest position for each chromosome
+  maxposition <- sapply(1:max(ordered$Chromosome),function(x){ max(which(ordered$Chromosome == x))})
+  ggplot(ordered)+
+    geom_point(aes(x= Reorder, y= -log10(P.value), color = Chromosome))+
+    scale_x_continuous(label = 1:max(ordered$Chromosome), breaks = maxposition)+
+    scale_color_viridis(option = "viridis")+
+    geom_hline(yintercept = thresholdline, linetype = "dashed", color = "grey")+
+    xlab("Genomic location")+
     ylab("-log10(P)")
 }
-
-ordered <- dplyr::arrange(yieldGWAS,Chromosome,Position)
-ordered$Reorder <- as.numeric(rownames(ordered))
-# use sapply to get all biggest position for each chromosome
-maxposition <- sapply(1:max(ordered$Chromosome),function(x){ max(which(ordered$Chromosome == x))})
-ggplot(ordered)+
-  geom_point(aes(x= Reorder, y= -log10(P.value), color = Chromosome))+
-  scale_x_continuous(label = 1:max(ordered$Chromosome), breaks = maxposition)+
-  scale_color_viridis(option = "viridis")+
-  xlab("Genomic location")+
-  ylab("-log10(P)")
 
